9ef82f4763c5bd2976773dcd8911d1fc955a3426
M Minhaj <eb20103087.minhajwahid@gmail.com>
2023-09-19 09:05:29 -0700

    "done"

--- app.js
+++ app.js
@@ -183,4 +183,172 @@ function rot13(str) {
   return str.split("").map(char => cracker[char]).join("");
 }
 
+<<<<<<< HEAD
 console.log(rot13("SERR PBQR PNZC!"));
+=======
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// Confirm the Ending algo 
+function confirmEnding(str, target) {
+  var len = target.length
+  var len2 = str.length 
+  var newStr = str.slice(len2-len,len2)
+  if(newStr==target){
+    return true
+  }else{
+    return false
+  }
+}
+
+console.log(confirmEnding("Bastian sasome", "some"));
+
+// Repeat a String Repeat a String algo 
+function repeatStringNumTimes(str, num) {
+  var newStr2 = ""
+  for (let i =0;i<num;i++){
+    newStr2 += str
+  }
+  return newStr2
+}
+
+console.log(repeatStringNumTimes("abc", 3));
+
+// Truncate a String algo 
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+truncateString("t basket",4)
+
+// Title Case a Sentence algo
+function titleCase(str) {
+  const newTitle = str.split(" ");
+  const updatedTitle = [];
+  for (let st in newTitle) {
+    updatedTitle[st] = newTitle[st][0].toUpperCase() + newTitle[st].slice(1).toLowerCase();
+  }
+  return updatedTitle.join(" ");
+}
+
+titleCase("I'm a little baby")
+// Sum All Numbers in a Range algo
+function sumAll(arr) {
+    var temp=0
+    if(arr[0]>arr[1]){
+        var num = arr[0]
+        for(let i=1;i<=num;i++){
+            temp+=i
+        }
+    }else{
+        var num = arr[1]
+        for(let i=1;i<=num;i++){
+            temp+=i
+        }
+    }
+    return temp;
+}
+  
+console.log(sumAll([5, 10]));
+
+//destroyer
+function destroyer(arr) {
+  const valsToRemove = Object.values(arguments).slice(1);
+  const filteredArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let removeElement = false;
+    for (let j = 0; j < valsToRemove.length; j++) {
+      if (arr[i] === valsToRemove[j]) {
+        removeElement = true;
+      }
+    }
+    if (!removeElement) {
+      filteredArray.push(arr[i]);
+    }
+  }
+  return filteredArray;
+}
+
+//sum of all prime numbers
+function sumPrimes(num) {
+  // Helper function to check primality
+  function isPrime(num) {
+    const sqrt = Math.sqrt(num);
+    for (let i = 2; i <= sqrt; i++) {
+      if (num % i === 0)
+        return false;
+    }
+    return true;
+  }
+
+  // Check all numbers for primality
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i))
+      sum += i;
+  }
+  return sum;
+}
+
+//find mising letters
+function fearNotLetter(str) {
+  for (let i = 0; i < str.length; i++) {
+    /* code of current character */
+    const charCode = str.charCodeAt(i);
+
+    /* if code of current character is not equal to first character + no of iteration
+        then a letter was skipped */
+    if (charCode !== str.charCodeAt(0) + i) {
+      /* if current character skipped past a character find previous character and return */
+      return String.fromCharCode(charCode - 1);
+    }
+  }
+  return undefined;
+}
+
+// test here
+fearNotLetter("abce");
+
+//Wherefore art thou
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  const collectionMatches = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    let foundMismatch = false;
+
+    for (const sourceProp in source) {
+      if (collection[i][sourceProp] !== source[sourceProp]) {
+        foundMismatch = true;
+      }
+    }
+    if (!foundMismatch) {
+      collectionMatches.push(collection[i]);
+    }
+  }
+  return collectionMatches;
+}
+
+Sum All Odd Fibonacci Numbers
+function sumFibs(num) {
+  let prevNumber = 0;
+  let currNumber = 1;
+  let result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
+}
+
+// test here
+sumFibs(4);
+>>>>>>> 33ba6b06e5f884af7e1a373b4cfc054d1e8d8d67
